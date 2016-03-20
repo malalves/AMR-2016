99d0dd5264f24eef2afc24b48daad9f66940c6be
malalves <malacrida01@gmail.com>
2016-03-20 11:57:07 -0300

    new controler

--- src/controler.cpp
+++ src/controler.cpp
@@ -6,32 +6,32 @@
 #define b 0.35
 #define r 0.1
 
-void S_calc(std_msgs::Float64 &vr, std_msgs::Float64 &vl,double v, double w){
-    vr.data = v - ((b/2)*w);
-    vl.data = v + ((b/2)*w);
+ros::Publisher pubR;
+ros::Publisher pubL;
+
+
+void c_callback(const geometry_msgs::TwistConstPtr &tws){
+	std_msgs::Float64 vr, vl;
+
+	ROS_INFO("received twist %f %f",tws->angular.z,tws->linear.x);
+
+    vr.data = tws->linear.x - ((b/2)*(tws->angular.z));
+    vl.data = tws->linear.x + ((b/2)*(tws->angular.z));
+
+	pubR.publish(vr);
+	pubL.publish(vl);
 }
     
 int main(int argc, char **argv){
-    double v=1, w=0;
-    std_msgs::Float64 vr, vl;
     
     ros::init(argc,argv,"control");
     
     ros::NodeHandle control;
+
+	ros::Subscriber twistSub = control.subscribe("/vehicle/Twist",1,c_callback);
     
-    ros::Publisher pubR = control.advertise<std_msgs::Float64>("/vrep/vehicle/motorRightSpeed", 1);
-    ros::Publisher pubL = control.advertise<std_msgs::Float64>("/vrep/vehicle/motorLeftSpeed", 1);
+    pubR = control.advertise<std_msgs::Float64>("/vrep/vehicle/motorRightSpeed", 1);
+    pubL = control.advertise<std_msgs::Float64>("/vrep/vehicle/motorLeftSpeed", 1);
     
-    ros::Rate loop_rate(10);
-    while(ros::ok()){
-	   ROS_INFO("insira v e w");
-	   std::cin >> v >> w;	
-	
-	   S_calc(vr,vl , v, w);
-	   pubR.publish(vr);
-	   pubL.publish(vl);
-	
-	   ros::spinOnce();
-	   loop_rate.sleep();
-    }
+    ros::spin();
 }
